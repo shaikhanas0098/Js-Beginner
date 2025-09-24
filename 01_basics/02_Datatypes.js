a92a831ebfb34803f3ce02d90043dd19ea5d3342
shaikhanas0098 <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-24 19:26:52 +0000

    container config done

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// (3 + 3) // we are using nodejs, not browser
+
+
+console.log(3 +3)
+// code readability should be high
+console.log("Mohammad Anas")
+
+
+let name = "Mohammad Anas"
+let age = 20
+let isLoggedIn = true
+let state;
+
+// number => 2 the power 53
+// bigint
+// sring => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefinded
+console.log(typeof null); // object
+
+
