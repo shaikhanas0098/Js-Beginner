c08d6a45de3ae9a315208e8baeb298f7f4492a43
shaikhanas0098 <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-25 06:34:21 +0000

    Conversion operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -27,3 +27,30 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// ****************************** Opeartions **************************//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "Anas"
+let str3 = str1+ str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
