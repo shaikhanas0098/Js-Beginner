3ebea57b0b71cd4cd37b7f6268d0efafc08d2b19
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-26 16:58:21 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,8 +2,14 @@
 // Object.creates
 
 // object literals
+
+const mySym = Symbol("key1")
+
+
 const JsUser = {
     name: "Anas",
+    "full name": "Mohammad Anas",
+    [mySym]: "mykey1",
     age: 20,
     location: "Lucknow",
     email: "Anas@gmail.com",
@@ -11,4 +17,28 @@ const JsUser = {
     lastLoginindDays : ["Monday", "Saturday"]
 }
 
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
 
+JsUser.email = "Anas@chatgpt.com"
+// Object.freeze(JsUser) // freeze the object so that it cannot be modified
+JsUser.email = "anas@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JsUser");
+    
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JsUser, ${this.name}`);
+    
+}
+
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
+
+
+
+ 
