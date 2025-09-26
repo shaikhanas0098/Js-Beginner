79836bc0d230c26dc8214316de3f6a77d74150cb
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-26 17:24:25 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,24 @@
+// const tinderUser = new Object();
+const tinderUser = {}
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false; 
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userFullname: {
+            firstname: "Mohammad",
+            lastname: "Anas"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userFullname.firstname);
+
+const obj1 = {1: "a", 2: "b"};
+const obj2 = {3: "c", 4: "d"};
+
