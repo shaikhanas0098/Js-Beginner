82c0cf7181e5914a8b14ce89c0ba723746a4f2c5
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-26 10:57:20 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+// singleton object
+// Object.creates
+
+// object literals
+const JsUser = {
+    name: "Anas",
+    age: 20,
+    location: "Lucknow",
+    email: "Anas@gmail.com",
+    isLoggedIn: false,
+    lastLoginindDays : ["Monday", "Saturday"]
+}
+
+
