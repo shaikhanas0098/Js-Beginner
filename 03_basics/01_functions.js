666953f0c41e61b897d34957cc9746c1f0d8e5c8
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-27 18:02:19 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,4 +32,34 @@ function loginUserMessage(username = "Zayn"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Anas"))
+// console.log(loginUserMessage("Anas"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: "Anas",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    username: "Zayn",
+    price: 299
+})
+
+const myNewArray = [200, 400, 100, 500]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
