e2d5496b65756b635c5f49c490213be3144b8dcf
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-27 05:42:10 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("A");
+    console.log("N");
+    console.log("A");
+    console.log("S");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2)
+}
+
+addTwoNumbers(3, 4)
