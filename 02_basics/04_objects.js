c3cd61a033f9d931bf2982fecb0b68c5aad78ac9
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-27 05:06:16 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -22,3 +22,34 @@ const regularUser = {
 const obj1 = {1: "a", 2: "b"};
 const obj2 = {3: "c", 4: "d"};
 
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+    {
+        id: 1,
+        email: "A@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
