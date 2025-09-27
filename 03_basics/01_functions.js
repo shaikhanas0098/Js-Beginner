603a1c59eefdc47e0fd99cae7e390bdd9301d3bf
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-27 17:47:24 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -7,8 +7,29 @@ function sayMyName(){
 
 // sayMyName()
 
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+
+// }
+
 function addTwoNumbers(number1, number2){
-    console.log(number1 + number2)
+    
+    let result = number1 + number2
+    return result
+    
 }
 
-addTwoNumbers(3, 4)
+const result = addTwoNumbers(3, 4)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "Zayn"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+        
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Anas"))
