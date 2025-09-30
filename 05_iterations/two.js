3ce5c6c434672826a49ad1f5e0a0d1c0463d7392
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-30 04:17:04 +0000

    iterations

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+let index = 0
+while (index < 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["flash", "superman", "batman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let Score = 11
+
+do {
+    console.log(`Score is ${Score}`);
+    Score++
+    
+} while (Score <= 10);
