01b200411afd93d8d7c942757cc963a9b26622fe
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-09-30 04:34:48 +0000

    maps

---
+++ 05_iterations/three.js
@@ -0,0 +1,27 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr){
+    // console.log(num);
+    
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`);
+    
+}
+
+// Maps
+
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('Fr', "France")
+
+console.log(map);
