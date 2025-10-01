e7b722b60d01e37b02516cd78b7272d63b52f382
Mohd Anas <163311346+shaikhanas0098@users.noreply.github.com>
2025-10-01 03:34:17 +0000

    iterations

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -12,7 +12,7 @@ for (const num of arr){
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
     
 }
 
@@ -24,4 +24,21 @@ map.set('IN', "India")
 map.set('USA', "United States Of America")
 map.set('Fr', "France")
 
-console.log(map);
+// console.log(map);
+
+
+    for(const [key, value] of map){
+        // console.log(key, ':-', value);
+        
+    }
+
+    const myObj = {
+        'game1' : 'NFS',
+        'game2' : 'spiderman'
+    }
+
+    // for (const[key, value] of myObj) {
+    //     console.log(key, ':-', value); 
+        
+    // }
+
